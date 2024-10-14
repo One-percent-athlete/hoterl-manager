2047d7115a367ecf760f1fc71cbc2c1d68175a8a
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-14 19:08:53 +0900

    Modify gallery on homepage

--- src/components/HomeGallery.js
+++ src/components/HomeGallery.js
@@ -2,53 +2,45 @@
 
 export default function HomeGallery() {
     return (
-      <section className="container mt-5 mb-5">
+      <section className="container mt-5">
           <h3 className="mt-5 mb-5 text-center">Gallery</h3>
               <div className="row text-center">
-                  <div className="col-4">
-                      <div class="card" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
-                      <img src="banners/banner1.jpg" className="img-thumbnail"/>
-                          <div class="card-body">
-                          </div>
-                      </div>
-                  </div>
-                  <div className="col-4">
-                      <div class="card" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
-                      <img src="banners/banner4.jpg" className="img-thumbnail"/>
-                          <div class="card-body">
-                          </div>
-                      </div>
-                  </div>
-                  <div className="col-4">
-                      <div class="card" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
-                      <img src="banners/banner3.jpg" className="img-thumbnail"/>
-                          <div class="card-body">
-                      </div>
-                      </div>
-                  </div>
-                  <div className="col-4">
-                      <div class="card" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
-                      <img src="banners/banner5.jpg" className="img-thumbnail"/>
-                          <div class="card-body">
-                      </div>
-                      </div>
-                  </div>
-                  <div className="col-4">
-                      <div class="card" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
-                      <img src="banners/banner1.jpg" className="img-thumbnail"/>
-                          <div class="card-body">
-                      </div>
-                      </div>
-                  </div>
-                  <div className="col-4">
-                      <div class="card" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
-                      <img src="banners/banner2.jpg" className="img-thumbnail"/>
-                          <div class="card-body">
-                      </div>
-                      </div>
-                  </div>
+              <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+                    <img src="banners/banner3.jpg" className="img-thumbnail"/>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+                    <img src="banners/banner5.jpg" className="img-thumbnail"/>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+                    <img src="banners/banner4.jpg" className="img-thumbnail"/>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+                    <img src="banners/banner2.jpg" className="img-thumbnail"/>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+                    <img src="banners/banner3.jpg" className="img-thumbnail"/>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+                    <img src="banners/banner1.jpg" className="img-thumbnail"/>
+                    </div>
+                </div>
+                </div>
               </div>
       </section>
+
+      
     );
   }
   
