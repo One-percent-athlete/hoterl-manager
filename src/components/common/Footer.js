259137b1d97068bc64a492455c8011386e38263d
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-14 19:43:13 +0900

    Modify footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,50 +1,56 @@
-export default function Header() {
+import {Pacifico} from 'next/font/google';
+const pacifico = Pacifico({ subsets: ['latin'], weight:"400" })
+
+export default function Footer() {
     return (
-        <div className="container">
-            <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-                <div className="col mb-3">
-                <a href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
-                Hotel Manager</a>
-                <p className="text-body-secondary">© 2024</p>
-                </div>
-            
-                <div className="col mb-3">
-            
-                </div>
-            
-                <div className="col mb-3">
-                <h5>Section</h5>
-                <ul className="nav flex-column">
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                </ul>
-                </div>
-            
-                <div className="col mb-3">
-                <h5>Section</h5>
-                <ul className="nav flex-column">
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                </ul>
-                </div>
-            
-                <div className="col mb-3">
-                <h5>Section</h5>
-                <ul className="nav flex-column">
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                </ul>
-                </div>
-            </footer>
+        <div className="p-0 m-0" style={{'backgroundColor':'#416284'}} data-bs-theme="dark">
+            <div className="container">
+                <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+                    <div className="col mb-3">
+                    <a href="/" className={`{d-flex align-items-center mb-3 link-body-emphasis text-decoration-none text-info fs-3 fw-bolder navbar-brand ${pacifico.className}`}>
+                    Hotel Manager</a>
+                    
+                    <p className="text-body-secondary">© 2024</p>
+                    </div>
+                
+                    <div className="col mb-3">
+                
+                    </div>
+                
+                    <div className="col mb-3">
+                    <h5 className="text-white">Section</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+                    </ul>
+                    </div>
+                
+                    <div className="col mb-3">
+                    <h5 className="text-white">Section</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+                    </ul>
+                    </div>
+                
+                    <div className="col mb-3">
+                    <h5 className="text-white">Section</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+                    </ul>
+                    </div>
+                </footer>
+            </div>
         </div>
     );
   }
