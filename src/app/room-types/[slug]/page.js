64c9df2bfc284ea805c20c7e6df00a7f655f0337
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-21 22:12:55 +0900

    Add simple room details page

---
+++ src/app/room-types/[slug]/page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <section className="container my-5">
+      <h3 className="my-5 text-center">Room Details</h3>
+    </section>
+  )
+}
