6a1c2c7012627d05520cd1c684a8b48acc5126ed
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-21 22:08:20 +0900

    Modify room types page

--- src/app/room-types/page.js
+++ src/app/room-types/page.js
@@ -3,45 +3,87 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <section className="container my-5">
-      <h3 className="my-5 text-center">Services</h3>
+      <h3 className="my-5 text-center">Room Types (6)</h3>
       <div className="row text-center">
-        <div className="col-4">
+        <div className="col-4 mb-4">
           <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
-            <Link href="room-types" className="text-white">
+            <Link href="/room-types/" className="text-white">
               <img src="banners/banner2.jpg" className="img-thumbnail" />
             </Link>
             <div className="card-body hms-bg-dark text-white">
               <h5 className="card-title">
-                <Link href="room-types" className="text-white">
-                  Type 1
+                <Link href="/room-types/single" className="text-white">
+                  Single
                 </Link>
               </h5>
             </div>
           </div>
         </div>
-        <div className="col-4">
+        <div className="col-4 mb-4">
           <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
-            <Link href="room-types" className="text-white">
+            <Link href="/room-types/" className="text-white">
               <img src="banners/banner3.jpg" className="img-thumbnail" />
             </Link>
             <div className="card-body hms-bg-dark text-white">
               <h5 className="card-title">
-                <Link href="room-types" className="text-white">
-                  Type 2
+                <Link href="/room-types/double" className="text-white">
+                  Double
                 </Link>
               </h5>
             </div>
           </div>
         </div>
-        <div className="col-4">
+        <div className="col-4 mb-4">
           <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
-            <Link href="room-types" className="text-white">
+            <Link href="/room-types/" className="text-white">
               <img src="banners/banner1.jpg" className="img-thumbnail" />
             </Link>
             <div className="card-body hms-bg-dark text-white">
               <h5 className="card-title">
-                <Link href="room-types" className="text-white">
-                  Type 3
+                <Link href="/room-types/twin" className="text-white">
+                  Twin
+                </Link>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 mb-4">
+          <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
+            <Link href="/room-types/" className="text-white">
+              <img src="banners/banner3.jpg" className="img-thumbnail" />
+            </Link>
+            <div className="card-body hms-bg-dark text-white">
+              <h5 className="card-title">
+                <Link href="/room-types/delux" className="text-white">
+                  Delux
+                </Link>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 mb-4">
+          <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
+            <Link href="/room-types/" className="text-white">
+              <img src="banners/banner1.jpg" className="img-thumbnail" />
+            </Link>
+            <div className="card-body hms-bg-dark text-white">
+              <h5 className="card-title">
+                <Link href="/room-types/queen" className="text-white">
+                  Queen
+                </Link>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 mb-4">
+          <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
+            <Link href="/room-types/" className="text-white">
+              <img src="banners/banner2.jpg" className="img-thumbnail" />
+            </Link>
+            <div className="card-body hms-bg-dark text-white">
+              <h5 className="card-title">
+                <Link href="/room-types/king" className="text-white">
+                  King
                 </Link>
               </h5>
             </div>
