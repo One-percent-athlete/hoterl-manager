4c6dd228584208dda37997d687592dcfd5a539d8
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-23 22:06:03 +0900

    Create forget password page

--- src/app/user/forgetpassword/page.js
+++ src/app/user/forgetpassword/page.js
@@ -10,26 +10,24 @@ export default function Page() {
               <img src="/banners/2.jpg" className="img-fluid" />
             </div>
             <div className="col-md-6 col-12 mb-3">
-              <h3 className="mb-3">Login</h3>
+              <h3 className="mb-3">Change Password</h3>
               <div class="row">
                 <div class="col-12 mb-3">
-                  <label className="form-label">Username</label>
-                  <input type="text" class="form-control" />
+                  <label className="form-label">New Password</label>
+                  <input type="password" class="form-control" />
                 </div>
                 <div class="col-12 mb-3">
-                  <label className="form-label">Password</label>
+                  <label className="form-label">Confirm Password</label>
                   <input type="password" class="form-control" />
                 </div>
                 <div className="col-md-6 col-12 mb-3">
-                  <button className="btn hms-bg-dark">Login</button>
+                  <button className="btn hms-bg-dark">Submit</button>
                 </div>
                 <p>
-                  <Link className="text-danger" href="/user/forgetpassword">
-                    Forget password?
-                  </Link>
+                  Not a user yet? <Link href="/user/signup">Sign up</Link>
                 </p>
                 <p>
-                  Not a user yet? <Link href="/user/signup">Sign up</Link>
+                  Return to login? <Link href="/user/login">Login</Link>
                 </p>
               </div>
             </div>
