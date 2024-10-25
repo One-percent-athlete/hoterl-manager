7f77530d321bf430f37702fb916e3a29e725b9a3
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-25 15:19:57 +0900

    Modify payment logs page

--- src/app/user/paylogs/page.js
+++ src/app/user/paylogs/page.js
@@ -10,7 +10,35 @@ export default function Page() {
           <DashboardSidebar />
         </div>
         <div className="col-md-8 col-12">
-          <h3>Payment Logs</h3>
+          <div className="table-responsive">
+            <table className="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>Payment Date</th>
+                  <th>Details</th>
+                  <th>Invoice</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>11/02/2024</td>
+                  <td>
+                    <p className="m-0">Num of Rooms: 2</p>
+                    <p className="m-0">Days of Stay: 3</p>
+                    <p className="m-0">Num of Guests: 2</p>
+                  </td>
+                  <td>
+                    <p className="m-0">
+                      <Link href="#">Invoice</Link>
+                    </p>
+                    <p className="m-0">
+                      Total Cost: 15,000 <small className="text-success">paid</small>
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </section>
