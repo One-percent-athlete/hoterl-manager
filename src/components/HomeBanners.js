02bc71567f94648a9925ef43526d1bc78b914894
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-30 09:02:04 +0900

    Create function to fetch data from backend

--- src/components/HomeBanners.js
+++ src/components/HomeBanners.js
@@ -1,8 +1,20 @@
 'use client'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Carousel } from 'react-responsive-carousel'
+import { useState, useEffect } from 'react'
 
 export default function HomeBanners() {
+  const [bannerList, setBannerList] = useState([])
+  async function fetchData() {
+    const banners = await fetch('http://127.0.0.1:8000/api/banners')
+    const res = await banners.json()
+    setBannerList(res)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <Carousel>
       <div>
