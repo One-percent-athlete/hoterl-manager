2254e6df8217269949b068d0f74ccafdcf36aa7b
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-01 15:28:31 +0900

    Update room detail to fetch data from backend api

--- src/app/rooms/[slug]/page.js
+++ src/app/rooms/[slug]/page.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import RoomImages from '@/components/RoomBanners'
 
-async function getData(room_type_id) {
-  const res = await fetch('http://127.0.0.1:8000/api/room_types/' +room_type_id, {cache: "no-store"})
+async function getData(uuid) {
+  const res = await fetch('http://127.0.0.1:8000/api/room_types/' + uuid, {cache: "no-store"})
   const data = await res.json()
   return data
 }
-export default async function Page() {
-  const room_type_id = 1
-  const roomDetails = await getData(room_type_id)
+export default async function Page({params}) {
+  const uuid = params.slug
+  const roomDetails = await getData(uuid)
   return (
     <section className="container my-5">
       <h3 className="my-5 text-center">{roomDetails.title} Room</h3>
@@ -17,19 +17,19 @@ export default async function Page() {
           <RoomImages />
           <h4 className="my-5">Amenities</h4>
           <div className="col-5">
-            <p>
-              <i className="fa fa-check-circle text-success"></i> Free wifi
+            <p> 
+              <i className="fa fa-check-circle text-success"></i>&ensp;{roomDetails.details[1]}
             </p>
             <p>
-              <i className="fa fa-check-circle text-success"></i> Welcome drink
+              <i className="fa fa-check-circle text-success"></i>&ensp;{roomDetails.details[2]}
             </p>
             <p>
-              <i className="fa fa-check-circle text-success"></i> Drinking water
+              <i className="fa fa-check-circle text-success"></i>&ensp;{roomDetails.details[3]}
             </p>
           </div>
           <h4 className="my-5">Location</h4>
           <div className="col-5">
-            <p>{roomDetails.details.location}</p>
+            <p>{roomDetails.details[0]}</p>
           </div>
         </div>
         <div className="col-7">
--- src/components/RoomTypeCard.js
+++ src/components/RoomTypeCard.js
@@ -4,12 +4,12 @@ export default function RoomTypeCard({ item }) {
   return (
     <div className="col-4 mb-4">
       <div className="card" style={{ 'backgroundColor': '#416284' }} data-bs-theme="dark">
-        <Link href={`/rooms/${item.title}`} className="text-white">
+        <Link href={`/rooms/${item.uuid}`} className="text-white">
           <img src="banners/banner2.jpg" className="img-thumbnail" />
         </Link>
         <div className="card-body hms-bg-dark text-white">
           <h5 className="card-title">
-            <Link href={`/rooms/${item.title}`} className="text-white">
+            <Link href={`/rooms/${item.uuid}`} className="text-white">
               {item.title}
             </Link>
           </h5>
