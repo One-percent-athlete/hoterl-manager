3f8faf43f416abd137df02e447aa7e0f3e669bc9
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-02 11:28:23 +0900

    Add function to the sign up form

--- src/app/user/signup/page.js
+++ src/app/user/signup/page.js
@@ -1,43 +1,50 @@
 import Link from 'next/link'
 
 export default function Page() {
+
+  async function handleForm(formData){
+    "use server"
+    console.log(formData);
+    
+  }
+
   return (
     <section className="container my-5">
       <div className="row">
         <div className="col-10 offset-1">
+              <h3 className="mb-5 text-center">Signup</h3>
           <div className="row">
             <div className="col-md-6 col-12">
               <img src="/banners/banner1.jpg" className="img-fluid" />
             </div>
             <div className="col-md-6 col-12 mb-3">
-              <h3 className="mb-3">Signup</h3>
-              <div className="row">
+              <form className="row" action={handleForm}>
                 <div className="col-md-6 col-12 mb-3">
                   <label className="form-label">First Name</label>
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" required name="first_name" />
                 </div>
                 <div className="col-md-6 col-12 mb-3">
                   <label className="form-label">Last Name</label>
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" required name="last_name" />
                 </div>
 
                 <div className="col-md-6 col-12 mb-3">
                   <label className="form-label">Username</label>
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" required name="username" />
                 </div>
                 <div className="col-md-6 col-12 mb-3">
                   <label className="form-label" aria-required>
                     Email
                   </label>
-                  <input type="email" className="form-control" />
+                  <input type="email" className="form-control" required name="email" />
                 </div>
                 <div className="col-12 mb-3">
                   <label className="form-label">Password</label>
-                  <input type="password" className="form-control" />
+                  <input type="password" className="form-control" required name="password1" />
                 </div>
                 <div className="col-12 mb-3">
                   <label className="form-label">Confirm Password</label>
-                  <input type="password" className="form-control" />
+                  <input type="password" className="form-control" required name="password2" />
                 </div>
                 <div className="col-md-6 col-12 mb-3">
                   <button className="btn btn-secondary">Reset</button>
@@ -46,7 +53,7 @@ export default function Page() {
                 <p>
                   Already a user? <Link href="/user/login">Login</Link>
                 </p>
-              </div>
+              </form>
             </div>
           </div>
         </div>
