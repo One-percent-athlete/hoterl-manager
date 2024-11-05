39a7981fdef62fa873904bdd780a9aee42a9861e
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-05 17:48:51 +0900

    Add function to booking form

--- src/app/rooms/BookingForm.js
+++ src/app/rooms/BookingForm.js
@@ -1,7 +1,45 @@
 "use client"
+import { useEffect, useState } from "react"
 import Link from 'next/link'
 
 export default function BookingForm({roomDetails}) {
+
+    const [totalCost, setTotalCost] = useState(0)
+    const [totalGuest, setTotalGuest] = useState(1)
+    const [checkinDate, setCheckinDate] = useState("")
+    const [checkoutDate, setCheckoutDate] = useState("")
+
+    const [checkinDateNormal, setCheckinDateNormal] = useState("")
+    const [checkoutDateNormal, setCheckoutDateNormal] = useState("")
+
+    function checkoutDateHandler(e) {
+        const _date=e.target.value
+        var _dateObj=new Date(e.target.value)
+        setCheckoutDate(_dateObj)
+        setCheckoutDateNormal(_date)
+    }
+
+    function checkinDateHandler(e) {
+        const _date=e.target.value
+        var _dateObj=new Date(_date)
+        setCheckinDate(_dateObj)
+        setCheckinDateNormal(_date)
+    }
+
+    function guestHandler(e) {
+        const _guest=e.target.value
+        setTotalGuest(_guest)
+    }
+
+    useEffect(() => {
+        const totalDays=Math.ceil(checkoutDate-checkinDate)/(1000*60*60*24)
+        var _totalCost=totalDays*roomDetails.price_per_night*totalGuest
+        console.log(totalGuest, checkinDate, checkoutDate, _totalCost);
+        setTotalCost(_totalCost)
+    },[checkinDateNormal, checkoutDateNormal])
+
+
+    
   var user = localStorage.getItem("user")
   user = JSON.parse(user)
   var bookingUrl= window.location.pathname
@@ -17,7 +55,7 @@ export default function BookingForm({roomDetails}) {
         </div>
         <div className="mb-3">
             <label htmlFor="exampleFormControlInput1" className="form-label">
-            <b>Cost:</b>　$ {roomDetails.price_per_night} / night
+            <b>Cost:</b>　$ {roomDetails.price_per_night} /person /night
             </label>
         </div>
         <hr />
@@ -25,19 +63,25 @@ export default function BookingForm({roomDetails}) {
             <label htmlFor="exampleFormControlInput1" className="form-label">
             <b>Total Guest</b>
             </label>
-            <input type="number" className="form-control" />
+            <input type="number" className="form-control" name="total_guest" onChange={guestHandler} value={totalGuest}/>
         </div>
         <div className="mb-3">
             <label htmlFor="exampleFormControlInput1" className="form-label">
             <b>Check In</b>
             </label>
-            <input type="date" className="form-control" />
+            <input type="date" className="form-control" name="checkin_date" onChange={checkinDateHandler} value={checkinDateNormal} />
         </div>
         <div className="mb-3">
             <label htmlFor="exampleFormControlInput1" className="form-label">
             <b>Check Out</b>
             </label>
-            <input type="date" className="form-control" />
+            <input type="date" className="form-control" name="checkout_date" onChange={checkoutDateHandler} value={checkoutDateNormal} />
+        </div>
+        <hr/>
+        <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+            <b>Total Charge:</b>　$ {totalCost}
+            </label>
         </div>
         <Link href="/rooms/?page=1" className="btn btn-secondary me-2" type="button">
             Back To Room Types
